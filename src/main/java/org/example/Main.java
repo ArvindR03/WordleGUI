8b84727221be4d491cb471bacb2d3542a6042208
Arvind Raghu <41343538+ArvindR03@users.noreply.github.com>
2022-11-21 21:29:03 +0000

    Finished supo 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
+// import org.example.Supo1;
+import org.example.Supo2.Question9.*;
 
 public class Main {
     public static void main(String[] args) {
-        // implementation of question 6
-        int a=0;
-        int b=2;
-        int[] res = Supo1.vectorAdd(a,b,1,1);
-        a = res[0];
-        b = res[1];
-        System.out.println((a+", "+b));
-        Supo1.Stack s = new Supo1.Stack(new int[] {1,2,3});
-        Supo1.LinkedList ll = new Supo1.LinkedList(5);
+
+        // implemented a shop where we have milk, a manager of a hr department and a shop assistant.
+        // printed the experience of the manager, and added a discount, then printed whether there is one or not and the new price
+
+        // prints "junior", "1.89", "true", "0.00188..."
+        
+        Shop sainsburys = new Shop("Sainsbury's", "27 Eddington Avenue, Cambridge, CB3 1SE");
+        sainsburys.addDepartment(new Department("HR", sainsburys));
+        sainsburys.addDepartment(new Department("customer service", sainsburys));
+        List<Shop> shops = new ArrayList<Shop>();
+        shops.add(sainsburys);
+        sainsburys.addItem(new Item("milk", 1.89F, false, shops));
+        sainsburys.getDepartments().get(0).setManager(new Manager("Baylee Neal", "hr manager", 100000, sainsburys.getDepartments().get(0), "junior"));
+        sainsburys.getDepartments().get(0).addAssistant(new Assistant("Faustina Das", "cashier", 2000, sainsburys.getDepartments().get(1)));
+
+        System.out.println(sainsburys.getDepartments().get(0).getManager().getExperience()); ;
+        System.out.println(sainsburys.getItems().get(0).getValue());
+
+        sainsburys.getItems().get(0).addDiscount(0.999F);
+        System.out.println(sainsburys.getItems().get(0).getDiscount());
+        System.out.println(sainsburys.getItems().get(0).getValue());
+
     }
 }
-
-//gitlab key glpat-NjCTjzP__nQ2HtaVycxY
